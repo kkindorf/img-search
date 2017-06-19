284302b06dbb0e8258cc3b7d3faa5d0cec213b05
kkindorf <kevin.kindorf@coresecure.com>
2017-06-19 16:45:40 -0400

    upating config file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === "production" ?
-                        "mongodb://kkindorf88:Zooniebin976@ds133192.mlab.com:33192/img-search" :
+                        "mongodb://kkindorf88:Zooniebin9765@ds133192.mlab.com:33192/img-search" :
                         "mongodb://localhost/img-search");
 
 exports.PORT = process.env.PORT || 8080;
